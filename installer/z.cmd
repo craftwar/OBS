f01aeab56b892880670059b4d04f7ed198373098
craftwar <craftwar@users.noreply.github.com>
2017-12-30 15:07:30 +0800

    Add z.cmd to clean up unnecessary files

---
+++ installer/z.cmd
@@ -0,0 +1,18 @@
+mkdir Z_backup\locale
+for %%i in (%cd%\locale\*) do if not %%~nxi == en.txt move /Y %%i Z_backup\locale\
+::for %%i in (%cd%\locale\*) do if not %%~nxi == en.txt del %%i
+:: for %i in (%cd%\\plugins\*) do if not %i == GraphicsCapture* move /Y %i Z_backup\
+move /Y plugins\dshowplugin Z_backup\plugins
+move /Y plugins\dshowplugin.dll Z_backup\plugins
+move /Y plugins\noisegate.dll Z_backup\plugins
+move /Y plugins\psvplugin Z_backup\plugins
+move /Y plugins\psvplugin.dll Z_backup\plugins
+move /Y plugins\scenesw Z_backup\plugins
+move /Y plugins\scenesw.dll Z_backup\plugins
+move /Y ObsNvenc.dll Z_backup
+move /Y QSVHelper.exe Z_backup
+
+::graphicscapture
+::graphicscapture.dll
+::move /Y plugins\
+::Z_backup
